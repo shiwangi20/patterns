f952db3bd1c32719cd89b6374585d075424e9d86
samgarg7009 <samikshagarg321@gmail.comm>
2022-10-30 20:55:09 +0530

    factory pattern

---
+++ factory_pattern.js
@@ -0,0 +1,36 @@
+function Developler(name) {
+    this.name = name;
+    this.type = "developer"
+}
+
+function Tester(name) {
+    this.name = name;
+    this.type = "tester"
+}
+
+function EmployeeFactory() {
+    this.create = (name, type) => {
+        switch (type) {
+            case 1:
+                return new Developler(name)
+                break;
+            case 2:
+                return new Tester(name)
+                break;
+        }
+    }
+}
+
+function say() {
+    console.log("hi i am " + this.name + " and I am " + this.type)
+}
+
+const employeeFactory = new EmployeeFactory()
+const employees = []
+
+employees.push(employeeFactory.create("sam", 1))
+employees.push(employeeFactory.create("tom", 2))
+
+employees.forEach(emp => {
+    say.call(emp)
+})
